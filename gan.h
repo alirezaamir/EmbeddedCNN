98a8fe725f66791b6ac927fafcde5d6f588b7c5a
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2021-02-17 17:45:10 +0100

    clean the code for the implementation

--- gan.h
+++ gan.h
@@ -5,18 +5,15 @@
 #ifndef EPILEPSYGAN_GAN_H
 #define EPILEPSYGAN_GAN_H
 
-#define NUM_FRACTION_BITS 32
+#define NUM_FRACTION_BITS 30
 
-#define mem2d(data,data_len,j,i)   data[((j)*(data_len))+(i)]
 #define MUL(x, y) (int)((((long)(x)*(long)(y)))>>NUM_FRACTION_BITS)
 
-// data is a 2-dimensional matrix implemented as 1-dimensional array
-// data[y][x] == data[ y * q + x ]
-
+#define mem2d(data,data_len,j,i)   data[((j)*(data_len))+(i)]
 #define mem3d(filter,filter_len,filter_depth,n,k,i)   filter[((n)*(filter_depth)+(k))*(filter_len)+(i)]
 
-#define LEAKY_RATIO 322122547 // 32bits: 1288490188 // 40 bits: 329853488332 // 30bits: 322122547
-#define INV_LEAKY_RATIO 3579139413// 32bits: 14316557653 // 40 bits: 3665038758886 // 30 bits: 3579139413
+#define LEAKY_RATIO 322122547
+#define INV_LEAKY_RATIO 3579139413
 
 extern int Z_array[];
 extern int* enc_w[8];
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gan.h"
-//#include "gan.c"
 
 #define FILTER_SIZE 31
 
@@ -20,16 +19,10 @@ void forward_propagation(int   *data);
 // =================================================================================
 
 int main() {
-    printf("EpilepsyGAN!\n");
     // allocate memory in CPU for calculation
     int   *eeg_input;
-    const int in_len = 2048;
-    // fill a, b matrices with random values between -16.0f and 16.0f
-//    srand(0);
-//    fill_map(eeg_input, in_len, 1);
     eeg_input = input_array;
     forward_propagation(eeg_input);
-//    free(eeg_input);
     return 0;
 }
 
@@ -45,21 +38,12 @@ void conv1d(const int   *data, const int   *filter, int   *map_out,
                     if (start_index + w_i - 15 < input_len && start_index + w_i - 15 > -1) {
                         sum += MUL(mem3d(filter, FILTER_SIZE, input_depth, w_n, w_j, w_i),
                                mem2d(data, input_len, w_j, start_index + w_i - 15));
-//                        printf("weight %d,%d,%d X in %d, %d: %d X %d = %d\n",
-//                               w_i, w_j, w_n, start_index + w_i - 15,  w_j,
-//                               mem3d(filter, FILTER_SIZE, input_depth, w_n, w_j, w_i),
-//                               mem2d(data, input_len, w_j, start_index + w_i - 15),
-//                               MUL(mem3d(filter, FILTER_SIZE, input_depth, w_n, w_j, w_i),
-//                                   mem2d(data, input_len, w_j, start_index + w_i - 15)));
                     }
                 }
             }
             if (sum < 0)
                 sum = MUL(sum, LEAKY_RATIO); // Leaky Relu
             mem2d(map_out, input_len >> 1, w_n, start_index >> 1) = sum;
-//            printf("Wrote %f in %d, %d\n", sum, start_index >> 1, w_n );
-//            if (input_depth > 1)
-//                getchar();
         }
     }
 }
@@ -77,7 +61,6 @@ void deconv1d(int   *data, const int   *filter, int   *map_out,
             else
                 upsampled[i] = 0;
         }
-//    free(data);
 
     // Convolution
     int   sum;
@@ -90,11 +73,6 @@ void deconv1d(int   *data, const int   *filter, int   *map_out,
                     if (start_index + w_i - 15 < input_len && start_index + w_i - 15 > -1) {
                         sum += MUL(mem3d(filter, FILTER_SIZE, n_filter, w_j, w_n, FILTER_SIZE - w_i - 1) ,
                                mem2d(upsampled, input_len, w_j, start_index + w_i - 15));
-//                        printf("weight %d,%d,%d X in %d, %d: %d X %d = %d\n",
-//                               FILTER_SIZE - w_i - 1, w_j, w_n, start_index + w_i - 15, w_j,
-//                               mem3d(filter, FILTER_SIZE, n_filter, w_j, w_n, FILTER_SIZE - w_i - 1),
-//                               mem2d(upsampled, input_len, w_j, start_index + w_i - 15),
-//                               sum);
                     }
                 }
             }
@@ -110,8 +88,8 @@ void concatenate(const int   *data, const int   *z, int   *map_out,
                  int input_len, int input_depth) {
     for (int w_j = 0; w_j < input_depth; w_j++) {
         for (int w_i = 0; w_i < input_len; w_i++) {
-            mem2d(map_out, input_len, w_j, w_i) = 0;
-//            mem2d(map_out, input_len, w_j + input_depth, w_i) = mem2d(z, input_len, w_j, w_i);
+//            mem2d(map_out, input_len, w_j, w_i) = 0;
+            mem2d(map_out, input_len, w_j + input_depth, w_i) = mem2d(z, input_len, w_j, w_i);
             mem2d(map_out, input_len, w_j + input_depth, w_i) = mem2d(data, input_len, w_j, w_i);
         }
     }
@@ -122,67 +100,50 @@ void skip_add(int   *data, int   *filter, int   *map_out,
               int input_len, const int input_depth) {
     for (int w_j = 0; w_j < input_depth; w_j++) {
         for (int w_i = 0; w_i < input_len; w_i++) {
-//            printf("%d, %d\n", w_j, w_i);
-//            printf("enc: %f\n", mem2d(data, input_len, w_j, w_i));
-//            printf("A: %f\n", mem2d(filter, input_len, w_j, w_i));
-//            printf("dec: %f\n", mem2d(map_out, input_len, w_j, w_i));
             int   add = MUL(mem2d(data, input_len, w_j, w_i), mem2d(filter, input_len, w_j, w_i));
             add += mem2d(data, input_len, w_j, w_i);
             if (add <0)
                 add = MUL(add,  INV_LEAKY_RATIO);
             mem2d(map_out, input_len, w_j, w_i) += add;
-//            getchar();
         }
     }
-//    free(data);
-//    free(filter);
 }
 
 
-void save_file(int   *data, char *filename, int input_len) {
-    FILE *fp;
-    fp = fopen(filename, "w");
-    for (int wi = 0; wi < input_len; wi++)
-        fprintf(fp, "%d\n", data[wi]);
-
-    fclose(fp);
-}
+//void save_file(int   *data, char *filename, int input_len) {
+//    FILE *fp;
+//    fp = fopen(filename, "w");
+//    for (int wi = 0; wi < input_len; wi++)
+//        fprintf(fp, "%d\n", data[wi]);
+//
+//    fclose(fp);
+//}
 
 void forward_propagation(int   *data) {
     int   *encoder_layers_out[8] = {0};
     int depth_size[9] = {1, 64, 64, 128, 128, 256, 256, 512, 1024};
     int map_size[9] = {2048, 1024, 512, 256, 128, 64, 32, 16, 8};
-//    float enc_ratio[8] = {1.2602388f, 0.2659299f, 0.1959376f, 0.13729893f, 0.1064626f, 0.0913380f, 0.08377741f, 0.05075028f};
-//    float dec_ratio[8] = {0.06047365f, 0.06849701f, 0.19342509f, 0.3388827f, 0.4225882f, 0.4932240f, 0.5388081f, 2.3255081f};
 
     int   *filter;
     int   *layer_in = (int   *) data;
-    save_file(layer_in, "input.txt", map_size[0]);
 
     // Encoder
     for (int layer = 0; layer <= 7; layer++) {
-        printf("Encoder Layer %d\n", layer);
         encoder_layers_out[layer] = (int   *) malloc(map_size[layer + 1] * depth_size[layer + 1] * sizeof(int  ));
         filter = enc_w[layer];
         conv1d(layer_in, filter, encoder_layers_out[layer], map_size[layer], depth_size[layer],
                depth_size[layer + 1]);
         layer_in = encoder_layers_out[layer];
-//        free(filter);
-        char out_filename[17];
-        sprintf(out_filename, "fxd_enc_out%d.txt", layer);
-        save_file(encoder_layers_out[layer], out_filename, map_size[layer + 1] * depth_size[layer + 1]);
     }
 
     // Concatenation
     layer_in = (int   *) malloc(map_size[8] * depth_size[8] * 2 * sizeof(int  ));
     concatenate(encoder_layers_out[7], Z_array, layer_in, map_size[8], depth_size[8]);
-    save_file(layer_in, "fxd_concatenate.txt", map_size[8] * depth_size[8] * 2 );
 
     // Decoder
     int   *decoder_layers_out;
     int   *skip;
     for (int layer = 7; layer >= 0; layer--) {
-        printf("Decoder Layer %d\n", layer);
         decoder_layers_out = (int   *) malloc(map_size[layer] * depth_size[layer] * sizeof(int  ));
         filter = dec_w[7 - layer];
         deconv1d(layer_in, filter, decoder_layers_out, map_size[layer + 1],
@@ -192,11 +153,6 @@ void forward_propagation(int   *data) {
             skip_add(encoder_layers_out[layer-1], skip, decoder_layers_out, map_size[layer], depth_size[layer]);
         }
         layer_in = decoder_layers_out;
-        char out_filename[17];
-        sprintf(out_filename, "fxd_dec_out%d.txt", 7 - layer);
-        save_file(decoder_layers_out, out_filename, map_size[layer] * depth_size[layer]);
-//        free(filter);
     }
-
-    save_file(layer_in, "fxd_output.txt", map_size[0]);
+//    save_file(layer_in, "fxp_output.txt", map_size[0]);
 }
