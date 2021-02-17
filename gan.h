e823b3f4c453e0b2a11e064e312b2da662d94444
Amirshahi Alireza <amirshah@eslpc3.intranet.epfl.ch>
2021-02-17 10:46:13 +0100

    use 2's complement with 32 bits

--- gan.h
+++ gan.h
@@ -5,10 +5,10 @@
 #ifndef EPILEPSYGAN_GAN_H
 #define EPILEPSYGAN_GAN_H
 
-#define NUM_FRACTION_BITS 30
+#define NUM_FRACTION_BITS 32
 
 #define mem2d(data,data_len,j,i)   data[((j)*(data_len))+(i)]
-#define MUL(x, y) ((((long)(x)*(long)(y)))>>NUM_FRACTION_BITS)
+#define MUL(x, y) (int)((((long)(x)*(long)(y)))>>NUM_FRACTION_BITS)
 
 // data is a 2-dimensional matrix implemented as 1-dimensional array
 // data[y][x] == data[ y * q + x ]
@@ -18,10 +18,10 @@
 #define LEAKY_RATIO 322122547 // 32bits: 1288490188 // 40 bits: 329853488332 // 30bits: 322122547
 #define INV_LEAKY_RATIO 3579139413// 32bits: 14316557653 // 40 bits: 3665038758886 // 30 bits: 3579139413
 
-extern long Z_array[];
-extern long* enc_w[8];
-extern long* dec_w[8];
-extern long* A_w[7];
-extern long input_array[];
+extern int Z_array[];
+extern int* enc_w[8];
+extern int* dec_w[8];
+extern int* A_w[7];
+extern int input_array[];
 
 #endif //EPILEPSYGAN_GAN_H
