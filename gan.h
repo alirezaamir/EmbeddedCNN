7170f7a693524f639a76ca57c64a52f4960e16ce
Alireza Amirshahi <a.amirshahi37@gmail.com>
2021-08-13 15:58:16 +0200

    debug the implementation with 32bit integers

--- gan.h
+++ gan.h
@@ -5,19 +5,19 @@
 #ifndef EPILEPSYGAN_GAN_H
 #define EPILEPSYGAN_GAN_H
 
-#define NUM_FRACTION_BITS 12
+#define NUM_FRACTION_BITS 26
 
-#define MUL(x, y) (short)((((int)(x)*(int)(y)))>>NUM_FRACTION_BITS)
+#define MUL(x, y) (int)((((long)(x)*(long)(y)))>>NUM_FRACTION_BITS)
 
 #define mem2d(data,data_len,j,i)   data[((j)*(data_len))+(i)]
 #define mem3d(filter,filter_len,filter_depth,n,k,i)   filter[((n)*(filter_depth)+(k))*(filter_len)+(i)]
 
-extern short* enc_w[9];
-extern short* bias_w[9];
-extern short* skip_w[4];
-extern short* skip_bias[4];
-extern short fc_weights_1[];
-extern short fc_bias_1[];
-extern short input_array[];
+extern int* enc_w[9];
+extern int* bias_w[9];
+extern int* skip_w[4];
+extern int* skip_bias[4];
+extern int fc_weights_1[];
+extern int fc_bias_1[];
+extern int input_array[];
 
 #endif //EPILEPSYGAN_GAN_H
--- main.c
+++ main.c
@@ -6,32 +6,32 @@
 // ===========================> Functions Prototype <===============================
 //void conv1d(int *data, int *filter, int *map_out, int *bias, int filter_size,
 //            int input_len, int input_depth, int n_filter, int strides);
-void conv1d(const short *data, const short *filter, short *map_out, const short *bias, int filter_size,
+void conv1d(const int *data, const int *filter, int *map_out, const int *bias, int filter_size,
             int input_len, int input_depth, int output_len, int n_filter, int strides);
 
         void deconv1d(int *data, const int *filter, int *map_out,
               int input_len, int input_depth, int n_filter);
 
-void skip_add(const short *data, short *map_out, int input_len);
+void skip_add(const int *data, int *map_out, int input_len);
 
-void forward_propagation(short *data);
+void forward_propagation(int *data);
 // =================================================================================
 
 int main() {
     // allocate memory in CPU for calculation
-    short *ecg_input;
+    int *ecg_input;
     ecg_input = input_array;
     forward_propagation(ecg_input);
     return 0;
 }
 
 
-void conv1d(const short *data, const short *filter, short *map_out,const short *bias, const int filter_size,
+void conv1d(const int *data, const int *filter, int *map_out,const int *bias, const int filter_size,
             const int input_len, const int input_depth, const int output_len, const int n_filter, const int strides) {
     int sum;
     int mult;
     for (int w_n = 0; w_n < n_filter; w_n++) {
-        for (int start_index = 0; start_index < input_len-filter_size; start_index += strides) {
+        for (int start_index = 0; start_index <= input_len-filter_size; start_index += strides) {
             sum = 0;
             for (int w_i = 0; w_i < filter_size; w_i++) {
                 for (int w_j = 0; w_j < input_depth; w_j++) {
@@ -47,12 +47,12 @@ void conv1d(const short *data, const short *filter, short *map_out,const short *
     }
 }
 
-void max1d(const short *data, short *map_out, const int input_len, const int input_depth,
+void max1d(const int *data, int *map_out, const int input_len, const int input_depth,
            const int pool_size, const int strides) {
     int out_len = (input_len - pool_size / 2) / strides;
     for (int start_index = 0; start_index < input_len - pool_size; start_index += strides) {
         for (int w_j = 0; w_j < input_depth; w_j++) {
-            short maximum = mem2d(data, input_len, w_j, start_index);
+            int maximum = mem2d(data, input_len, w_j, start_index);
             for (int w_i = 1; w_i < pool_size; w_i++) {
                 if (mem2d(data, input_len, w_j, start_index + w_i) > maximum)
                     maximum = mem2d(data, input_len, w_j, start_index + w_i);
@@ -62,13 +62,13 @@ void max1d(const short *data, short *map_out, const int input_len, const int inp
     }
 }
 
-void skip_add(const short *data, short *map_out, int input_len) {
+void skip_add(const int *data, int *map_out, int input_len) {
     for (int i = 0; i < input_len; i++)
         map_out[i] += data[i];
 }
 
 
-void save_file(short *data, char *filename, int input_len) {
+void save_file(int *data, char *filename, int input_len) {
     FILE *fp;
     fp = fopen(filename, "w");
     for (int wi = 0; wi < input_len; wi++)
@@ -77,30 +77,30 @@ void save_file(short *data, char *filename, int input_len) {
     fclose(fp);
 }
 
-void forward_propagation(short *data) {
+void forward_propagation(int *data) {
     int depth_size[11] = {1, 64, 64, 64, 64, 128, 128, 256, 256, 512, 512};
     int map_size[11] = {768, 381, 190,  94, 92, 45, 43, 21, 19, 9, 7};
 
-    short *filter;
-    short *bias;
-    short *layer_in = (short *) data;
+    int *filter;
+    int *bias;
+    int *layer_in = (int *) data;
     save_file(data, "input_data.txt", 768);
 
     //
-    short *first_conv = (short *) malloc(map_size[1] * depth_size[1] * sizeof(short));
+    int *first_conv = (int *) malloc(map_size[1] * depth_size[1] * sizeof(int));
     filter = enc_w[0];
     bias = bias_w[0];
     conv1d(layer_in, filter, first_conv, bias, 7, map_size[0], depth_size[0], map_size[1],
            depth_size[1], 2);
-    save_file(filter, "filter_conv1d_1.txt", 7 * depth_size[1]);
-    short *max_pool = (short *) malloc(map_size[2] * depth_size[2] * sizeof(short));
+    int *max_pool = (int *) malloc(map_size[2] * depth_size[2] * sizeof(int));
     max1d(first_conv, max_pool, map_size[1], depth_size[1], 3, 2);
+    save_file(max_pool, "max_pool1.txt", map_size[2] * depth_size[2]);
     //free(first_conv);
 
     layer_in = max_pool;
     for (int block = 0; block < 4; block++) {
         int index = block * 2 + 2;
-        short* conv1d_1 = (short *) malloc(map_size[index+1] * depth_size[index+1] * sizeof(short));
+        int* conv1d_1 = (int *) malloc(map_size[index+1] * depth_size[index+1] * sizeof(int));
         filter = enc_w[index-1];
         bias = bias_w[index-1];
         conv1d(layer_in, filter, conv1d_1, bias, 3, map_size[index], depth_size[index], map_size[index+1],
@@ -109,7 +109,7 @@ void forward_propagation(short *data) {
         sprintf(out_filename, "conv1d_b%d_1.txt", block);
         save_file(conv1d_1, out_filename, map_size[index + 1] * depth_size[index + 1]);
 
-        short *conv1d_2 = (short *) malloc(map_size[index+2] * depth_size[index+2] * sizeof(short));
+        int *conv1d_2 = (int *) malloc(map_size[index+2] * depth_size[index+2] * sizeof(int));
         filter = enc_w[index];
         bias = bias_w[index];
         conv1d(conv1d_1, filter, conv1d_2, bias, 3, map_size[index + 1], depth_size[index+1], map_size[index + 2],
@@ -117,7 +117,7 @@ void forward_propagation(short *data) {
         sprintf(out_filename, "conv1d_b%d_2.txt", block);
         save_file(conv1d_2, out_filename, map_size[index + 2] * depth_size[index + 2]);
 
-        short* conv1d_skip = (short *) malloc(map_size[index + 2] * depth_size[index + 2] * sizeof(short));
+        int* conv1d_skip = (int *) malloc(map_size[index + 2] * depth_size[index + 2] * sizeof(int));
         filter = skip_w[block];
         bias = skip_bias[block];
         conv1d(layer_in, filter, conv1d_skip, bias, 7, map_size[index], depth_size[index], map_size[index + 2],
@@ -125,10 +125,10 @@ void forward_propagation(short *data) {
 
         skip_add(conv1d_2, conv1d_skip, map_size[index + 2] * depth_size[index + 2]);
         sprintf(out_filename, "conv1d_b%d_3.txt", block);
-        save_file(conv1d_2, out_filename, map_size[index + 2] * depth_size[index + 2]);
-        layer_in = conv1d_2;
+        save_file(conv1d_skip, out_filename, map_size[index + 2] * depth_size[index + 2]);
+        layer_in = conv1d_skip;
     }
-    short *fully_connected = malloc(2 * sizeof(short));
+    int *fully_connected = malloc(2 * sizeof(int));
     conv1d(layer_in, fc_weights_1, fully_connected, fc_bias_1, 7, map_size[10], depth_size[10],
            1, 2, 1);
 
