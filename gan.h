1432d3902f812dfdf538651cc068b28b85557aac
Alireza Amirshahi <a.amirshahi37@gmail.com>
2021-08-31 14:39:29 +0200

    make all the weights short

--- gan.h
+++ gan.h
@@ -5,18 +5,20 @@
 #ifndef EPILEPSYGAN_GAN_H
 #define EPILEPSYGAN_GAN_H
 
-#define NUM_FRACTION_BITS 10
+#define NUM_FRACTION_DATA 10
+#define NUM_FRACTION_CNV_FC 8
+#define NUM_FRACTION_BN 5
 
-#define MUL(x, y) (short)(((int)(x)*(int)(y))>>NUM_FRACTION_BITS)
+#define MUL(x, y, num) (short)(((int)(x)*(int)(y))>>(num))
 
 #define mem2d(data,data_len,j,i)   data[((j)*(data_len))+(i)]
 #define mem3d(filter,filter_len,filter_depth,n,k,i)   filter[((n)*(filter_depth)+(k))*(filter_len)+(i)]
 
-extern short* conv1d_w[3];
-extern short* conv1d_b[3];
-extern short* dense_w[2];
-extern short* dense_b[2];
-extern short* bn[12];
+extern char* conv1d_w[3];
+extern char* conv1d_b[3];
+extern char* dense_w[2];
+extern char* dense_b[2];
+extern char* bn[12];
 extern short input_array[];
 
 #endif //EPILEPSYGAN_GAN_H
--- main.c
+++ main.c
@@ -4,16 +4,19 @@
 
 
 // ===========================> Functions Prototype <===============================
-void conv1d(const short *data, const short *filter, short *map_out, const short *bias, int filter_size,
+void conv1d(const short *data, const char *filter, short *map_out, const char *bias, int filter_size,
             int input_len, int input_depth, int output_len, int n_filter, int strides, int relu, int padding);
 
-void batch_normalization(const short *data, const short *gamma, const short *beta, const short *mean, const short *var,
+void batch_normalization(const short *data, const char *gamma, const char *beta, const char *mean, const char *var,
                          short *map_out, int input_len, int input_depth);
 
 void skip_add(const short *data, short *map_out, int input_len);
 
 short forward_propagation(short *data);
-void save_file(short *data, char *filename, int input_len) ;
+
+void save_file(short *data, char *filename, int input_len);
+
+void save_weight(char *data, char *filename, int input_len);
 // =================================================================================
 
 int main() {
@@ -21,23 +24,23 @@ int main() {
     short *eeg_input;
     short prediction[2000];
     eeg_input = input_array;
-    for (int sample=0; sample< 2000; sample++){
-        short predict = forward_propagation(eeg_input+sample*23*1024);
+    for (int sample = 0; sample < 2000; sample++) {
+        short predict = forward_propagation(eeg_input + sample * 23 * 1024);
         printf("%d: %d\n", sample, predict);
         prediction[sample] = predict;
     }
-    save_file(prediction, "2000_short.txt", 2000);
+    save_file(prediction, "2000_char.txt", 2000);
 
     return 0;
 }
 
 
-void conv1d(const short *data, const short *filter, short *map_out,const short *bias, const int filter_size,
+void conv1d(const short *data, const char *filter, short *map_out, const char *bias, const int filter_size,
             const int input_len, const int input_depth, const int output_len, const int n_filter, const int strides,
             const int relu, const int padding) {
     int sum;
     int mult;
-    int pad = padding? filter_size / 2 : 0;
+    int pad = padding ? filter_size / 2 : 0;
     for (int w_n = 0; w_n < n_filter; w_n++) {
         for (int start_index = 0; start_index < input_len; start_index += strides) {
             sum = 0;
@@ -45,7 +48,7 @@ void conv1d(const short *data, const short *filter, short *map_out,const short *
                 for (int w_j = 0; w_j < input_depth; w_j++) {
                     if (start_index + w_i - pad < input_len && start_index + w_i - pad > -1) {
                         mult = MUL(mem3d(filter, filter_size, input_depth, w_n, w_j, w_i),
-                                   mem2d(data, input_len, w_j, start_index + w_i - pad));
+                                   mem2d(data, input_len, w_j, start_index + w_i - pad), NUM_FRACTION_CNV_FC);
                         sum += mult;
                     }
                 }
@@ -53,7 +56,9 @@ void conv1d(const short *data, const short *filter, short *map_out,const short *
             sum += bias[w_n];
             if (sum < 0 && relu)
                 sum = 0; // Relu
-            mem2d(map_out, output_len, w_n, start_index/strides) = (short) sum;
+            if (sum > (1 << 15) || sum < -(1 << 15))
+                printf("Overflow %d\n", sum);
+            mem2d(map_out, output_len, w_n, start_index / strides) = (short) sum;
         }
     }
 }
@@ -73,12 +78,16 @@ void max1d(const short *data, short *map_out, const int input_len, const int inp
     }
 }
 
-void batch_normalization(const short *data, const short *gamma, const short *beta, const short *mean, const short *var,
-                         short *map_out, const int input_len, const int input_depth){
-    for (int start_index = 0; start_index < input_len; start_index ++) {
+void batch_normalization(const short *data, const char *gamma, const char *beta, const char *mean, const char *var,
+                         short *map_out, const int input_len, const int input_depth) {
+    for (int start_index = 0; start_index < input_len; start_index++) {
         for (int w_j = 0; w_j < input_depth; w_j++) {
-            int normalized = MUL(mem2d(data, input_len, w_j, start_index) - mean[w_j] , var[w_j]);
-            mem2d(map_out, input_len, w_j, start_index) = MUL(normalized , gamma[w_j]) + beta[w_j];
+            short normalized = mem2d(data, input_len, w_j, start_index) -
+                               ((short) mean[w_j] << (NUM_FRACTION_DATA - NUM_FRACTION_BN));
+            short standardized = MUL(normalized, var[w_j], NUM_FRACTION_BN);
+            short new_standardized = MUL(standardized, gamma[w_j], NUM_FRACTION_BN);
+            mem2d(map_out, input_len, w_j, start_index) = (short) (new_standardized +
+                    ((short) beta[w_j]<< (NUM_FRACTION_DATA - NUM_FRACTION_BN)));
         }
     }
 }
@@ -101,13 +110,13 @@ void save_file(short *data, char *filename, int input_len) {
 short forward_propagation(short *data) {
     int depth_size[6] = {23, 128, 128, 128, 100, 2};
     int map_size[6] = {1024, 256, 64, 16, 1, 1};
-    short *filter;
-    short *bias;
+    char *filter;
+    char *bias;
     short *layer_in = (short *) data;
 //    save_file(data, "input_data.txt", map_size[0] * depth_size[0]);
 
     for (int block = 0; block < 3; block++) {
-        short* conv1d_out = (short *) malloc(map_size[block] * depth_size[block + 1] * sizeof(short));
+        short *conv1d_out = (short *) malloc(map_size[block] * depth_size[block + 1] * sizeof(short));
         filter = conv1d_w[block];
         bias = conv1d_b[block];
         conv1d(layer_in, filter, conv1d_out, bias, 3, map_size[block], depth_size[block], map_size[block],
@@ -116,25 +125,25 @@ short forward_propagation(short *data) {
 //        sprintf(out_filename, "conv1d_%d.txt", block);
 //        save_file(conv1d_out, out_filename, map_size[block] * depth_size[block + 1]);
 
-        batch_normalization(conv1d_out, bn[block*4], bn[block*4+1], bn[block*4+2], bn[block*4+3],
-                            conv1d_out, map_size[block], depth_size[block+1]);
+        batch_normalization(conv1d_out, bn[block * 4], bn[block * 4 + 1], bn[block * 4 + 2], bn[block * 4 + 3],
+                            conv1d_out, map_size[block], depth_size[block + 1]);
 //        sprintf(out_filename, "bn_%d.txt", block);
 //        save_file(conv1d_out, out_filename, map_size[block] * depth_size[block + 1]);
 
         relu(conv1d_out, conv1d_out, map_size[block] * depth_size[block + 1]);
 
-        short* max_out = (short *) malloc(map_size[block+1] * depth_size[block + 1] * sizeof(short));
-        max1d(conv1d_out, max_out, map_size[block], depth_size[block+1], 4, 4);
+        short *max_out = (short *) malloc(map_size[block + 1] * depth_size[block + 1] * sizeof(short));
+        max1d(conv1d_out, max_out, map_size[block], depth_size[block + 1], 4, 4);
 //        sprintf(out_filename, "max_pool_%d.txt", block);
 //        save_file(max_out, out_filename, map_size[block + 1] * depth_size[block + 1]);
         layer_in = max_out;
     }
-    for (int fc_index=0; fc_index<2; fc_index++) {
-        short *fully_connected = malloc(map_size[4+fc_index] * depth_size[4+fc_index] * sizeof(short));
-        conv1d(layer_in, dense_w[fc_index], fully_connected, dense_b[fc_index], map_size[3+fc_index],
-               map_size[3+fc_index], depth_size[3+fc_index],
-               map_size[4+fc_index], depth_size[4+fc_index], map_size[3+fc_index],
-               fc_index<1 ? 1: 0, 0);
+    for (int fc_index = 0; fc_index < 2; fc_index++) {
+        short *fully_connected = malloc(map_size[4 + fc_index] * depth_size[4 + fc_index] * sizeof(short));
+        conv1d(layer_in, dense_w[fc_index], fully_connected, dense_b[fc_index], map_size[3 + fc_index],
+               map_size[3 + fc_index], depth_size[3 + fc_index],
+               map_size[4 + fc_index], depth_size[4 + fc_index], map_size[3 + fc_index],
+               fc_index < 1 ? 1 : 0, 0);
 //        char out_filename[17];
 //        sprintf(out_filename, "dense_%d.txt", fc_index);
 //        save_file(fully_connected, out_filename, map_size[4+fc_index] * depth_size[4+fc_index]);
