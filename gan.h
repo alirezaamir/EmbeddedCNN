5c31b4b4e133cb780d4a15d85be459d29c5711fc
Alireza Amirshahi <a.amirshahi37@gmail.com>
2021-09-08 11:36:51 +0200

    change variable types to __intx_t

--- gan.h
+++ gan.h
@@ -1,7 +1,8 @@
 //
 // Created by alireza on 1/7/21.
 //
-
+#include <stdio.h>
+#include <stdlib.h>
 #ifndef EPILEPSYGAN_GAN_H
 #define EPILEPSYGAN_GAN_H
 
@@ -15,11 +16,11 @@
 #define mem2d(data,data_len,j,i)   data[((j)*(data_len))+(i)]
 #define mem3d(filter,filter_len,filter_depth,n,k,i)   filter[((n)*(filter_depth)+(k))*(filter_len)+(i)]
 
-extern char* conv1d_w[3];
-extern char* conv1d_b[3];
-extern char* dense_w[2];
-extern char* dense_b[2];
-extern char* bn[12];
-extern short input_array[];
+extern __int8_t* conv1d_w[3];
+extern __int8_t* conv1d_b[3];
+extern __int8_t* dense_w[2];
+extern __int8_t* dense_b[2];
+extern __int8_t* bn[12];
+extern __int16_t input_array[];
 
 #endif //EPILEPSYGAN_GAN_H
--- main.c
+++ main.c
@@ -1,41 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "gan.h"
 
 
 // ===========================> Functions Prototype <===============================
-void conv1d(const short *data, const char *filter, short *map_out, const char *bias, int filter_size,
-            int input_len, int input_depth, int output_len, int n_filter, int strides, int relu, int padding);
+void conv1d(const __int16_t *data, const __int8_t *filter, __int16_t *map_out, const __int8_t *bias, __int32_t filter_size,
+            __int32_t input_len, __int32_t input_depth, __int32_t output_len, __int32_t n_filter, __int32_t strides, __int32_t relu, __int32_t padding);
 
-void conv_max1d(const short *data, const char *filter, short *map_out, const char *bias, int filter_size,
-            int input_len, int input_depth, int output_len, int n_filter, int strides, int relu, int padding, int pool_size);
+void conv_max1d(const __int16_t *data, const __int8_t *filter, __int16_t *map_out, const __int8_t *bias, __int32_t filter_size,
+            __int32_t input_len, __int32_t input_depth, __int32_t output_len, __int32_t n_filter, __int32_t strides, __int32_t relu, __int32_t padding, __int32_t pool_size);
 
-void batch_normalization(const short *data, const char *gamma, const char *beta, const char *mean, const char *var,
-                         short *map_out, int input_len, int input_depth);
+void batch_normalization(const __int16_t *data, const __int8_t *gamma, const __int8_t *beta, const __int8_t *mean, const __int8_t *var,
+                         __int16_t *map_out, __int32_t input_len, __int32_t input_depth);
 
-short forward_propagation(short *data);
+__int16_t forward_propagation(__int16_t *data);
 
 // =================================================================================
 
-int main() {
-    short* eeg_input = input_array;
-    short predict = forward_propagation(eeg_input);
+__int32_t main() {
+    printf("Input Array : %x\n", input_array[0]);
+    __int16_t* eeg_input = input_array;
+    __int16_t predict = forward_propagation(eeg_input);
     printf("Prediction : %d", predict);
     return 0;
 }
 
 
-void conv1d(const short *data, const char *filter, short *map_out, const char *bias, const int filter_size,
-            const int input_len, const int input_depth, const int output_len, const int n_filter, const int strides,
-            const int relu, const int padding) {
-    int sum;
-    int mult;
-    int pad = padding ? filter_size / 2 : 0;
-    for (int w_n = 0; w_n < n_filter; w_n++) {
-        for (int start_index = 0; start_index < input_len; start_index += strides) {
+void conv1d(const __int16_t *data, const __int8_t *filter, __int16_t *map_out, const __int8_t *bias, const __int32_t filter_size,
+            const __int32_t input_len, const __int32_t input_depth, const __int32_t output_len, const __int32_t n_filter, const __int32_t strides,
+            const __int32_t relu, const __int32_t padding) {
+    __int32_t sum;
+    __int32_t mult;
+    __int32_t pad = padding ? filter_size / 2 : 0;
+    for (__int32_t w_n = 0; w_n < n_filter; w_n++) {
+        for (__int32_t start_index = 0; start_index < input_len; start_index += strides) {
             sum = 0;
-            for (int w_i = 0; w_i < filter_size; w_i++) {
-                for (int w_j = 0; w_j < input_depth; w_j++) {
+            for (__int32_t w_i = 0; w_i < filter_size; w_i++) {
+                for (__int32_t w_j = 0; w_j < input_depth; w_j++) {
                     if (start_index + w_i - pad < input_len && start_index + w_i - pad > -1) {
                         mult = MUL(mem3d(filter, filter_size, input_depth, w_n, w_j, w_i),
                                    mem2d(data, input_len, w_j, start_index + w_i - pad), NUM_FRACTION_CNV_FC);
@@ -53,24 +52,24 @@ void conv1d(const short *data, const char *filter, short *map_out, const char *b
                 printf("Overflow %d\n", sum);
                 sum = -(1<<15) +1;
             }
-            mem2d(map_out, output_len, w_n, start_index / strides) = (short) sum;
+            mem2d(map_out, output_len, w_n, start_index / strides) = (__int16_t) sum;
         }
     }
 }
 
 
-void conv_max1d(const short *data, const char *filter, short *map_out, const char *bias, const int filter_size,
-            const int input_len, const int input_depth, const int output_len, const int n_filter, const int strides,
-            const int relu, const int padding, const int pool_size) {
-    int sum;
-    int mult;
-    int pad = padding ? filter_size / 2 : 0;
-    for (int w_n = 0; w_n < n_filter; w_n++) {
-        int maximum = NEG_INF;
-        for (int start_index = 0; start_index < input_len; start_index += strides) {
+void conv_max1d(const __int16_t *data, const __int8_t *filter, __int16_t *map_out, const __int8_t *bias, const __int32_t filter_size,
+            const __int32_t input_len, const __int32_t input_depth, const __int32_t output_len, const __int32_t n_filter, const __int32_t strides,
+            const __int32_t relu, const __int32_t padding, const __int32_t pool_size) {
+    __int32_t sum;
+    __int32_t mult;
+    __int32_t pad = padding ? filter_size / 2 : 0;
+    for (__int32_t w_n = 0; w_n < n_filter; w_n++) {
+        __int32_t maximum = NEG_INF;
+        for (__int32_t start_index = 0; start_index < input_len; start_index += strides) {
             sum = 0;
-            for (int w_i = 0; w_i < filter_size; w_i++) {
-                for (int w_j = 0; w_j < input_depth; w_j++) {
+            for (__int32_t w_i = 0; w_i < filter_size; w_i++) {
+                for (__int32_t w_j = 0; w_j < input_depth; w_j++) {
                     if (start_index + w_i - pad < input_len && start_index + w_i - pad > -1) {
                         mult = MUL(mem3d(filter, filter_size, input_depth, w_n, w_j, w_i),
                                    mem2d(data, input_len, w_j, start_index + w_i - pad), NUM_FRACTION_CNV_FC);
@@ -92,38 +91,38 @@ void conv_max1d(const short *data, const char *filter, short *map_out, const cha
                 maximum = sum;
             }
             if (start_index % pool_size == pool_size-1) {
-                mem2d(map_out, output_len, w_n, start_index / (strides * pool_size)) = (short) maximum;
+                mem2d(map_out, output_len, w_n, start_index / (strides * pool_size)) = (__int16_t) maximum;
                 maximum = NEG_INF;
             }
         }
     }
 }
 
-void batch_normalization(const short *data, const char *gamma, const char *beta, const char *mean, const char *var,
-                         short *map_out, const int input_len, const int input_depth) {
-    for (int start_index = 0; start_index < input_len; start_index++) {
-        for (int w_j = 0; w_j < input_depth; w_j++) {
-            short normalized = mem2d(data, input_len, w_j, start_index) -
-                               ((short) mean[w_j] << (NUM_FRACTION_DATA - NUM_FRACTION_BN));
-            short standardized = MUL(normalized, var[w_j], NUM_FRACTION_BN);
-            short new_standardized = MUL(standardized, gamma[w_j], NUM_FRACTION_BN);
-            mem2d(map_out, input_len, w_j, start_index) = (short) (new_standardized +
-                                                                   ((short) beta[w_j]
+void batch_normalization(const __int16_t *data, const __int8_t *gamma, const __int8_t *beta, const __int8_t *mean, const __int8_t *var,
+                         __int16_t *map_out, const __int32_t input_len, const __int32_t input_depth) {
+    for (__int32_t start_index = 0; start_index < input_len; start_index++) {
+        for (__int32_t w_j = 0; w_j < input_depth; w_j++) {
+            __int16_t normalized = mem2d(data, input_len, w_j, start_index) -
+                               ((__int16_t) mean[w_j] << (NUM_FRACTION_DATA - NUM_FRACTION_BN));
+            __int16_t standardized = MUL(normalized, var[w_j], NUM_FRACTION_BN);
+            __int16_t new_standardized = MUL(standardized, gamma[w_j], NUM_FRACTION_BN);
+            mem2d(map_out, input_len, w_j, start_index) = (__int16_t) (new_standardized +
+                                                                   ((__int16_t) beta[w_j]
                                                                            << (NUM_FRACTION_DATA - NUM_FRACTION_BN)));
         }
     }
 }
 
-void relu(const short *data, short *map_out, int input_len) {
-    for (int i = 0; i < input_len; i++)
+void relu(const __int16_t *data, __int16_t *map_out, __int32_t input_len) {
+    for (__int32_t i = 0; i < input_len; i++)
         map_out[i] = (data[i] < 0) ? 0 : data[i];
 }
 
-void conv_block(int block, short* layer_in, short* conv1d_out){
-    int depth_size[6] = {23, 128, 128, 128, 100, 2};
-    int map_size[6] = {1024, 256, 64, 16, 1, 1};
-    char* filter = conv1d_w[block];
-    char* bias = conv1d_b[block];
+void conv_block(__int32_t block, __int16_t* layer_in, __int16_t* conv1d_out){
+    __int32_t depth_size[6] = {23, 128, 128, 128, 100, 2};
+    __int32_t map_size[6] = {1024, 256, 64, 16, 1, 1};
+    int8_t* filter = conv1d_w[block];
+    __int8_t* bias = conv1d_b[block];
     conv_max1d(layer_in, filter, conv1d_out, bias, 3, map_size[block], depth_size[block], map_size[block+1],
                depth_size[block + 1], 1, 0, 1, 4);
 
@@ -133,15 +132,15 @@ void conv_block(int block, short* layer_in, short* conv1d_out){
     relu(conv1d_out, conv1d_out, map_size[block+1] * depth_size[block + 1]);
 }
 
-short forward_propagation(short *data) {
-    int fc_depth_size[6] = {128, 100, 2};
-    int fc_map_size[6] = {16, 1, 1};
-    short intermediate_map0[256*128];
-    short intermediate_map1[64*128];
+__int16_t forward_propagation(__int16_t *data) {
+    __int32_t fc_depth_size[6] = {128, 100, 2};
+    __int32_t fc_map_size[6] = {16, 1, 1};
+    __int16_t intermediate_map0[256*128];
+    __int16_t intermediate_map1[64*128];
 
     //  ************  BLOCK 0  ************ //
-    short *layer_out = intermediate_map0;
-    short *layer_in = (short *) data;
+    __int16_t *layer_out = intermediate_map0;
+    __int16_t *layer_in = (__int16_t *) data;
     conv_block(0, layer_in, layer_out);
 
     //  ************  BLOCK 1  ************ //
